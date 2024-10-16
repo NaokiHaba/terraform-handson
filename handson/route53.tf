dc3ce9ebf8acb500335077ac31bca849ee1bf95a
Naoki Haba <naosprintrunner6385@gmail.com>
2024-10-16 23:16:20 +0900

    feat: Update Route53 configuration for ACM certificate validation and ALB setup

--- handson/route53.tf
+++ handson/route53.tf
@@ -3,23 +3,27 @@ variable "domain" {
   default = "habas.click"
 }
 
-resource "aws_route53_zone" "main" {
-  name = var.domain
+# Route53 Hosted Zone
+data "aws_route53_zone" "main" {
+  private_zone = false
+  name         = "${var.domain}."
 }
 
+# ACM Certificate
 resource "aws_acm_certificate" "main" {
-  domain_name = "*.${var.domain}"
+  domain_name = var.domain
   validation_method = "DNS"
 
   tags = {
-    Environment = "dev"
+    Environment = "Dev"
   }
 
   lifecycle {
-    create_before_destroy = false
+    create_before_destroy = true
   }
 }
 
+# Route53 Record
 resource "aws_route53_record" "validation" {
   for_each = {
     for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
@@ -29,20 +33,26 @@ resource "aws_route53_record" "validation" {
     }
   }
 
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = aws_route53_zone.main.zone_id
+  depends_on = [aws_acm_certificate.main]
+
+  zone_id = data.aws_route53_zone.main.id
+  ttl     = 60
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
 }
 
+# ACM Certificate Validation
+resource "aws_acm_certificate_validation" "main" {
+  certificate_arn         = aws_acm_certificate.main.arn
+  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
+}
+
+# Route53 Record
 resource "aws_route53_record" "main" {
-  type = "A"
-
+  zone_id = data.aws_route53_zone.main.id
   name = var.domain
-  zone_id = aws_route53_zone.main.id
-
+  type = "A"
   alias {
     name = aws_lb.main.dns_name
     zone_id = aws_lb.main.zone_id
@@ -50,7 +60,8 @@ resource "aws_route53_record" "main" {
   }
 }
 
-resource "aws_lb_listener" "https" {
+# ALB Listener
+resource "aws_alb_listener" "https" {
   load_balancer_arn = aws_lb.main.arn
 
   certificate_arn = aws_acm_certificate.main.arn
@@ -60,42 +71,41 @@ resource "aws_lb_listener" "https" {
 
   default_action {
     type = "forward"
-    target_group_arn = aws_lb_target_group.main.arn
+    target_group_arn = aws_lb_target_group.main.id
   }
 }
 
-resource "aws_lb_listener_rule" "http_to_https" {
-  listener_arn = aws_lb_listener.https.arn
+# ALB Listener Rule
+resource "aws_alb_listener_rule" "http_to_https" {
+  listener_arn = aws_alb_listener.https.arn
 
   priority = 99
 
   action {
     type = "redirect"
-    
     redirect {
-      port        = "443"
-      protocol    = "HTTPS"
+      port = "443"
+      protocol = "HTTPS"
       status_code = "HTTP_301"
     }
   }
 
   condition {
     host_header {
-      values = [var.domain]
+      values = ["${var.domain}"]
     }
   }
 }
 
-resource "aws_security_group_rule" "alb_https" {
-  security_group_id = aws_security_group.ecs_tasks.id
+# Security Group Rule
+resource "aws_security_group_rule" "main" {
+  security_group_id = aws_security_group.alb.id
+
   type = "ingress"
   from_port = 443
   to_port = 443
+  
   protocol = "tcp"
+  
   cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_acm_certificate_validation" "main" {
-  certificate_arn         = aws_acm_certificate.main.arn
-  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
 }
